59b1fe2ab848f858cf8771ace02d341f3bd0f0e6
xfry <fry.andrade@gmail.com>
2019-03-05 19:52:20 +0000

    a malloc example

---
+++ malloc_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point.
+ *
+ * description: a function that create a dinamic array :D
+ * Return: integer lvalue
+ */
+int main(void)
+{
+	int n;
+	int i = 0;
+
+	printf("write the size of the array\n");
+	scanf("%d", &n);
+
+	int *Arr = (int *) malloc(n * sizeof(int));
+
+	for (i = 0; i < n; i++)
+	{
+		Arr[i] = i + 1;
+	}
+
+	for (int j = 0; j < n; j++)
+	{
+		printf("%d\n", Arr[j]);
+	}
+
+	return (0);
+}
